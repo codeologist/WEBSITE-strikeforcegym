d210aaa2837de5725e5557e52b78ff1470246ce8
codeology <coder451@codeology.co.nz>
2016-01-13 00:12:34 +1300

    added pricing and canterbury timetable, plus a smooth scroll for the menu selection

--- src/public/js/menu.js
+++ src/public/js/menu.js
@@ -1,8 +1,29 @@
 
+
 jQuery("document").ready(function($){
 
     var body = $('body');
     var bp = $("header").outerHeight(true);
+
+
+    $("#menu a").click( function( e ){
+
+        e.preventDefault();
+        var y, target = this.href.split("#")[1];
+
+        if ( target === "home"){
+            y=0;
+        } else {
+
+            y = $( "#" + target ).offset().top-60;
+
+        }
+
+        $("body").animate( {"scrollTop": y }, 1000 ,"easeInCirc");
+
+    });
+
+
 
     $(window).scroll(function () {
         if ($(this).scrollTop() > bp ) {
